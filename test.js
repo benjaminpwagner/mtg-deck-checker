75c7275f5cea2e8928a36a8cd77ad4d89f7784e3
Ben <benjaminpwagner@gmail.com>
2018-08-02 00:23:18 -0400

    vintage restricted list checking in progress

--- test.js
+++ test.js
@@ -4,10 +4,11 @@ const fs = require('fs');
 
 fs.readFile('decklist.txt', 'utf8', function(err, decklist) {
   if (err) throw err;
-  deck = new Deck.Deck(decklist, 'standard');
+  const format = 'legacy'
+  var deck = new Deck.Deck(decklist, format)
   console.log(deck.isLegal ?
-    `Decklist is legal for ${deck.format}!`
-    :`Illegal decklist for ${deck.format}!`
+    `Decklist is legal for ${format}!`
+    :`Illegal decklist for ${format}!`
   );
 });
 
