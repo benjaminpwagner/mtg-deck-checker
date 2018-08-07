bebd5e8143b2d253e787fdccd2e520d3af0eb709
Ben <benjaminpwagner@gmail.com>
2018-08-07 14:18:27 -0400

    hashing nearly done

--- Deck.js
+++ Deck.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const sha1 = require('sha1');
+const sha1 = require('js-sha1');
 
 function Deck(decklist, format) {
 
@@ -23,6 +23,8 @@ function Deck(decklist, format) {
   // default values
   // these values are reset by Deck.check()
   this.cards = {};
+  this.main = {};
+  this.side = {};
   this.size = 0;
   this.errors = [];
   this.isLegal = false;
@@ -31,7 +33,7 @@ function Deck(decklist, format) {
   this.check();
 }
 
-Deck.prototype.addCard = function(card, amount) {
+Deck.prototype.addCard = function(card, amount, inMain=true) {
   if (this.cards.hasOwnProperty(card)) {
     this.cards[card].amount += amount;
   } else {
@@ -39,27 +41,80 @@ Deck.prototype.addCard = function(card, amount) {
       amount: amount
     }
   }
+
+  if (inMain) {
+
+    if (this.main.hasOwnProperty(card)) {
+      this.main[card].amount += amount;
+    } else {
+      this.main[card] = {
+        amount: amount
+      }
+    }
+
+  } else {
+
+    if (this.side.hasOwnProperty(card)) {
+      this.side[card].amount += amount;
+    } else {
+      this.side[card] = {
+        amount: amount
+      }
+    }
+
+  }
+
 }
 
 Deck.prototype.parseList = function() {
   this.cards = {};
-  var toHash = [];
+  this.main = {};
+  this.side = {};
+  var regexData;
   this.list
     .split('\n')
     .forEach(line => {
       if(line !== '' && line.indexOf('//') === -1) {
-        const regexData = line.match(/[0-9]+/);
+        regexData = line.match(/[0-9]+/);
         var name = line.slice(regexData[0].length+regexData.index+1);
         var amount = parseInt(regexData[0])
-        this.addCard(name, amount);
-        toHash.push( line.indexOf('SB') === -1 ?
-          `${amount} ${name.toLowerCase()}`
-          :`SB:${amount} ${name.toLowerCase()}`
-        )
+        this.addCard( name, amount, line.indexOf('SB') === -1 );
       }
     });
-    console.log(toHash.sort().join('\n'))
-  console.log(sha1(toHash.join(';')))
+
+  var toHash = [];
+
+  for (card in this.main) {
+    if (this.main.hasOwnProperty(card)) {
+      for (var i=0; i<this.main[card].amount; i++) {
+        toHash.push(card.toLowerCase());
+      }
+    }
+  }
+
+  for (card in this.side) {
+    if (this.side.hasOwnProperty(card)) {
+      for (var i=0; i<this.side[card].amount; i++) {
+        toHash.push(`SB:${card.toLowerCase()}`);
+      }
+    }
+  }
+
+  toHash = toHash.sort();
+
+  console.log(toHash);
+
+  var sha = sha1.array( toHash.join(';') );
+  console.log(sha)
+  var byte1 = sha[0] << 32;
+  var byte2 = sha[1] << 24;
+  var byte3 = sha[2] << 16;
+  var byte4 = sha[3] << 8;
+  var byte5 = sha[4]
+  console.log(byte1, byte2, byte3, byte4, byte5);
+  console.log((byte1 + byte2 + byte3 + byte4 + byte5).toString(16));
+  console.log((byte1 + byte2 + byte3 + byte4 + byte5).toString(32));
+  console.log('558c3e67')
 }
 
 Deck.prototype.check = function(decklist=undefined,format=undefined) {
